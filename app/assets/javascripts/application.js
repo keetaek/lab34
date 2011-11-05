9fda8234ac006d1f49cd7ac01deba5fa2221ae26
keetaek <keetaek@gmail.com>
2011-11-05 16:02:17 -0700

    Added Flash player but development incomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,23 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+(function($){
+  var nav = $("#topNav");
+  nav.find("li").each(function() {
+    if($(this).find("ul").length > 0) {
+      $("<span>").text("^").appendTo($(this).children(":first"));
+      
+      //show subnav on hover
+      $(this).mouseenter(function() {
+        $(this).find("ul").stop(true, true).slideDown();
+      });
+      
+      //hide submenus on exit
+      $(this).mouseleave(function() {
+        $(this).find("ul").stop(true, true).slideUp();
+      });
+    }
+  })(jQuery);
+});
