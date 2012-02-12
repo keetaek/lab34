2030431770bfb40efd146bcce446ac9789d1eff0
Keetaek <keetaek@keetaek-VirtualBox.(none)>
2012-02-12 14:45:31 -0800

    Create Audition Step1 and Step2 mostly work except remove buttons

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,6 @@ class Role < ActiveRecord::Base
   belongs_to :audition
   has_many :applications
   has_many :medium, :through => :applications
-  has_many :role_questions
-  
+  has_many :role_questions, :dependent => :destroy
+  accepts_nested_attributes_for :role_questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
 end
