1121835030ddf4e75da96ae49fb8f979b305dfbc
Keetaek <keetaek@keetaek-VirtualBox.(none)>
2012-03-13 21:27:51 -0700

    Adding delete files

--- db/migrate/20111121025155_create_application_media_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateApplicationMediaJoinTable < ActiveRecord::Migration
-  def change
-    create_table :applications_medium, :id => false do |t|
-      t.integer :application_id
-      t.integer :media_id 
-    end
-  end
-end
--- db/migrate/20120213011613_add_email_to_audition_admins.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToAuditionAdmins < ActiveRecord::Migration
-  def change
-    add_column :audition_admins, :email, :string
-  end
-end
