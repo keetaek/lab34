8b78553cd4f712480c7cfc1d334054683fc16419
Keetaek <keetaek@keetaek-VirtualBox.(none)>
2012-05-28 19:15:08 -0700

    Organized CSS and JS files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,17 @@ $(function() {
     return this.optional(element) || postalcode.match(/(^\d{5}(-\d{4})?$)|(^[ABCEGHJKLMNPRSTVXYabceghjklmnpstvxy]{1}\d{1}[A-Za-z]{1} ?\d{1}[A-Za-z]{1}\d{1})$/);
   }, "Please specify a valid postal/zip code");
 });
+
+
+function remove_fields(link, field) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(field).remove();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
+}
+
+
