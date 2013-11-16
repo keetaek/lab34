6c157d1c3c2236558fc8f4fc9250fa62a89fe991
keetaek <keetaek@gmail.com>
2013-11-16 15:00:05 -0800

    adding logs

--- app/controllers/aws_sns_controller.rb
+++ app/controllers/aws_sns_controller.rb
@@ -10,10 +10,11 @@ class AwsSnsController < ApplicationController
       token = body["Token"]
       topic_arn = body["TopicArn"]
       client.confirm_subscription({:token => token, :topic_arn =>  topic_arn})
+      logger.info "Type Subscription \n body is #{body}"
     elsif message_type == "Notification"
       subject = body["Subject"]
       message = body["Message"]
-
+      logger.info "Type Notification \n body is #{body}"
     else
       # TODO - Shouldn't happen..
     end
